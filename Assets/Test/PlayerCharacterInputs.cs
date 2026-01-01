63a24c7415ade475cbd6683da360ea88129fa0cb
Hyun0801 <scoke0801@gmail.com>
2026-01-01 20:02:49 +0900

    KCC 테스트 중

---
+++ Assets/Test/PlayerCharacterInputs.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+// 1. 입력 데이터 구조체
+public struct PlayerCharacterInputs
+{
+    public float MoveAxisForward;
+    public float MoveAxisRight;
+    public Quaternion CameraRotation;
+    public bool JumpDown;
+}
---
+++ Assets/Test/PlayerInputHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public MyPlayerController Controller;
+    public Camera PlayerCamera;
+
+    private void Update()
+    {
+        if (Controller == null) return;
+
+        PlayerCharacterInputs inputs = new PlayerCharacterInputs();
+        inputs.MoveAxisForward = Input.GetAxisRaw("Vertical");
+        inputs.MoveAxisRight = Input.GetAxisRaw("Horizontal");
+        inputs.CameraRotation = PlayerCamera != null ? PlayerCamera.transform.rotation : Quaternion.identity;
+        inputs.JumpDown = Input.GetKeyDown(KeyCode.Space);
+
+        Controller.SetInputs(ref inputs);
+    }
+}

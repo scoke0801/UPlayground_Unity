b2bb952879da1acdeaf5090008d540150ffd8b0a
Hyun0801 <scoke0801@gmail.com>
2026-02-10 22:37:58 +0900

    적 테스트용

---
+++ Assets/02.Scripts/GameActor/Object/Monster/MonsterActor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+using UPlayGround.Data.Enum;
+using UPlayGround.Animation;
+using UPlayGround.Component;
+using UPlayGround.Data.Event;
+using UPlayGround.MovementController;
+using UPlayGround.Input;
+using UPlayGround.InputDefine;
+using UPlayGround.Manager;
+
+namespace UPlayGround
+{
+    public partial class MonsterActor : GameActor
+    {
+    }
+}
